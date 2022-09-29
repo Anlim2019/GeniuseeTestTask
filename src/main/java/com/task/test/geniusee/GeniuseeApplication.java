d723f19625930b3af4011b58eda6fbd64cf796e3
Oleksandr Tsikalov <otsikalov@exoft.net>
2022-09-29 11:20:32 +0300

    init

---
+++ src/main/java/com/task/test/geniusee/GeniuseeApplication.java
@@ -0,0 +1,13 @@
+package com.task.test.geniusee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeniuseeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GeniuseeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/task/test/geniusee/GeniuseeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.test.geniusee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeniuseeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
