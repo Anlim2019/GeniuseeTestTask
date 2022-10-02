22097784b6296918e8b330f643ea35b0c9b1a141
Oleksandr Tsikalov <otsikalov@exoft.net>
2022-10-02 16:27:09 +0300

    created controller

---
+++ src/main/java/com/task/test/geniusee/rest/MovieController.java
@@ -0,0 +1,56 @@
+package com.task.test.geniusee.rest;
+
+import com.task.test.geniusee.dto.MovieDto;
+import com.task.test.geniusee.entity.Movie;
+import com.task.test.geniusee.service.MovieService;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class MovieController {
+
+  private final MovieService movieService;
+
+  @PostMapping("/movie")
+  public ResponseEntity<MovieDto> create(@RequestBody MovieDto movie) {
+    return ResponseEntity.ok(this.movieService.create(movie));
+  }
+
+  @PutMapping("/movie")
+  public ResponseEntity<MovieDto> update(@RequestBody MovieDto movie) {
+    return ResponseEntity.ok(this.movieService.update(movie));
+  }
+
+  @GetMapping("/movie/{id}")
+  public ResponseEntity<MovieDto> getById(@PathVariable Long id) {
+    return ResponseEntity.ok(this.movieService.findById(id));
+  }
+
+  @GetMapping("/movies/{orderId}")
+  public ResponseEntity<List<MovieDto>> getAllByOrderId(@PathVariable Long orderId) {
+    return ResponseEntity.ok(this.movieService.findAllByOrderId(orderId));
+  }
+
+  @GetMapping("/movies")
+  public ResponseEntity<List<MovieDto>> getAll(@RequestParam(defaultValue = "0") Integer page,
+                                               @RequestParam(defaultValue = "3") Integer size) {
+    return ResponseEntity.ok(this.movieService.findAll(page, size));
+  }
+
+  @DeleteMapping("/movie/{id}")
+  public ResponseEntity<Void> removeById(@PathVariable Long id) {
+    this.movieService.removeById(id);
+    return ResponseEntity.ok().build();
+  }
+}
---
+++ src/main/java/com/task/test/geniusee/rest/OrderController.java
@@ -0,0 +1,52 @@
+package com.task.test.geniusee.rest;
+
+import com.task.test.geniusee.dto.MovieDto;
+import com.task.test.geniusee.dto.OrderDto;
+import com.task.test.geniusee.entity.Order;
+import com.task.test.geniusee.service.OrderService;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@AllArgsConstructor
+public class OrderController {
+  private final OrderService orderService;
+
+  @GetMapping("/order/{id}")
+  public ResponseEntity<OrderDto> getById(@PathVariable Long id) {
+    return ResponseEntity.ok(this.orderService.findById(id));
+  }
+
+  @GetMapping("/orders")
+  public ResponseEntity<List<OrderDto>> getAll(@RequestParam(defaultValue = "0") Integer page,
+                                             @RequestParam(defaultValue = "3") Integer size) {
+    return ResponseEntity.ok(this.orderService.findAll(page, size));
+  }
+
+  @PostMapping("/order")
+  public ResponseEntity<OrderDto> create(@RequestBody OrderDto orderDto) {
+    return ResponseEntity.ok(this.orderService.create(orderDto));
+  }
+
+  @PutMapping("/order")
+  public ResponseEntity<OrderDto> update(@RequestBody OrderDto orderDto) {
+    return ResponseEntity.ok(this.orderService.update(orderDto));
+  }
+
+  @DeleteMapping("/order/{id}")
+  public ResponseEntity<Void> removeById(@PathVariable Long id) {
+    this.orderService.removeById(id);
+    return ResponseEntity.ok().build();
+  }
+}
