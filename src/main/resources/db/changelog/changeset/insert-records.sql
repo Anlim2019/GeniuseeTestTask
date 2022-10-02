1db721099457e3a925a78a613f5cb5cc8bf97098
Oleksandr Tsikalov <otsikalov@exoft.net>
2022-10-02 16:26:25 +0300

    added liquibase migration

---
+++ src/main/resources/db/changelog/changeset/insert-records.sql
@@ -0,0 +1,26 @@
+INSERT INTO public.orders (id, price)
+VALUES (1, 544.3);
+
+INSERT INTO public.orders (id, price)
+VALUES (2, 123.3);
+
+INSERT INTO public.orders (id, price)
+VALUES (3, 332.3);
+
+INSERT INTO public.orders (id, price)
+VALUES (4, 232.3);
+
+INSERT INTO public.movies (name, rating, order_id)
+VALUES ('Pirates Of Caribbean', 9.1, 1);
+
+INSERT INTO public.movies (name, rating, order_id)
+VALUES ('Game of Thrones', 9.2, 2);
+
+INSERT INTO public.movies (name, rating, order_id)
+VALUES ('Captain America', 5.6, 1);
+
+INSERT INTO public.movies (name, rating, order_id)
+VALUES ('Soul', 9.4, 3);
+
+INSERT INTO public.movies (name, rating, order_id)
+VALUES ('Harry Potter', 9.4, 4);
