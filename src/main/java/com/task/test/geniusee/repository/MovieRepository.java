ea312f92b5b7bbaad1ad56212b90645e1e83fbab
Oleksandr Tsikalov <otsikalov@exoft.net>
2022-10-02 19:35:42 +0300

    updated repositories to maintain specifications

--- src/main/java/com/task/test/geniusee/repository/MovieRepository.java
+++ src/main/java/com/task/test/geniusee/repository/MovieRepository.java
@@ -2,9 +2,10 @@ package com.task.test.geniusee.repository;
 
 import com.task.test.geniusee.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MovieRepository extends JpaRepository<Movie, Long> {
+public interface MovieRepository extends JpaRepository<Movie, Long>, JpaSpecificationExecutor<Movie> {
 }
--- src/main/java/com/task/test/geniusee/repository/OrderRepository.java
+++ src/main/java/com/task/test/geniusee/repository/OrderRepository.java
@@ -1,9 +1,13 @@
 package com.task.test.geniusee.repository;
 
+import com.task.test.geniusee.entity.Movie;
 import com.task.test.geniusee.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository extends PagingAndSortingRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, Long>,
+    JpaSpecificationExecutor<Order> {
 }
